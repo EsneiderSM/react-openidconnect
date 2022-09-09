0c8bb49ec260894d0c1f75fd189ef74b819d11b9
esneider_serna <esneider.serna@doctus.com.co>
2022-09-09 14:53:44 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Authenticate from 'react-openidconnect';
+import { OidcSettings } from './oidcsettings';
+ 
+class App extends Component {
+ 
+  constructor(props) {
+    super(props);
+    this.userLoaded = this.userLoaded.bind(this); 
+    this.userUnLoaded = this.userUnLoaded.bind(this);
+ 
+    this.state = { user: undefined };
+  }  
+ 
+  userLoaded(user) {
+    if (user)
+      this.setState({ "user": user });
+  } 
+  
+  userUnLoaded() {
+    this.setState({ "user": undefined });
+  } 
+ 
+  NotAuthenticated() {
+    return <div>You are not authenticated, please click here to authenticate.</div>;
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  // @NOTE This function is optional. If you not pass it will looks for "#id_token" in the URL
+  checkAuthentication() {
+    return window.location.href.includes("my_token_hash"); 
+  }
+ 
+  render() {
+      return (
+        <Authenticate         
+          OidcSettings={OidcSettings} 
+          // checkAuthentication={checkAuthentication}
+          userLoaded={this.userLoaded} 
+          userunLoaded={this.userUnLoaded} 
+          renderNotAuthenticated={this.NotAuthenticated}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <div>If you see this you are authenticated.</div>
+        </Authenticate>
+      )
+  }
 }
 
 export default App;
